75c8304dfbb2aa4a740e912357c6cb24c175a652
3manuel0 <saiddimension@gmail.com>
2024-11-26 01:32:04 +0100

    styling

--- src/sections/Aboutme.tsx
+++ src/sections/Aboutme.tsx
@@ -3,14 +3,17 @@ function Aboutme() {
   return (
     <section className="about-me p-8">
       <h1 className="text-center">About Me</h1>
-      <div className="flex mt-4">
-        <div className="w-3/4 px-6">
-          <h2 className="mb-4">
-            Hi, My Name is Said Bennaji, AKA 3manuel{" "}
-            <span className="wave text-3xl">👋.</span>
+      <div className="flex mt-8 justify-center gap-8">
+        <div className="w-2/4 px-6">
+          <h2 className="mb-4 text-center">
+            Hi, My Name is Said AKA 3manuel{" "}
+            <span className="wave text-3xl">👋</span>
           </h2>
-          <p className="leading-8">
-            I'm a Full Stack Web Developer/Software Programmer from Morocco.
+          <p className="leading-6 text-center text-xs">
+            Full Stack Web Developer/Software Programmer
+          </p>
+          <p className="leading-6 text-center text-xs">
+            <span className="text-3xl map">🇲🇦</span> Morocco, Rabat
           </p>
         </div>
         <div className="w-1/4 ">
