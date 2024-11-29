a64d4f9bbfd0b8baa56a34789aa87703cf34bfe5
3manuel0 <saiddimension@gmail.com>
2024-11-29 03:38:18 +0100

    languages

--- src/sections/Aboutme.tsx
+++ src/sections/Aboutme.tsx
@@ -1,5 +1,19 @@
 import myImage from "../assets/personal-image/my-pic.jpg";
 function Aboutme() {
+  const gitContenLikn =
+    "https://raw.githubusercontent.com/3manuel0/3manuel0/assets/";
+  const logos = [
+    "C.svg",
+    "WebAssembly.svg",
+    "Python.svg",
+    "Javascript.svg",
+    "bash.svg",
+    "Mysql.svg",
+    "Git.svg",
+    "Nodejs.svg",
+    "Postgresql.svg",
+    "mongodb-icon-1.svg",
+  ];
   return (
     <section className="about-me p-8">
       <h1 className="text-center">About Me</h1>
@@ -9,23 +23,21 @@ function Aboutme() {
             Hi, My Name is Said AKA 3manuel{" "}
             <span className="wave text-3xl">👋</span>
           </h2>
-          <p className="leading-6 text-center text-xs">
+          <h2 className="leading-6 text-center text-xs">
             Full Stack Web Developer/Software Programmer
-          </p>
-          <p className="leading-6 text-center text-xs">
+          </h2>
+          <h2 className="leading-6 text-center text-xs">
             <span className="text-3xl map">🇲🇦</span> Morocco, Rabat
-          </p>
-          <div className="grid grid-cols-4 mt-8">
-            <img
-              className="icon"
-              src="https://raw.githubusercontent.com/3manuel0/3manuel0/assets/WebAssembly_Logo.svg"
-              alt=""
-            />
-            <img
-              className="icon"
-              src="https://raw.githubusercontent.com/3manuel0/3manuel0/assets/javascript-1.svg"
-              alt=""
-            />
+          </h2>
+          <div className="grid grid-cols-4 mt-8 gap-y-4 place-items-center">
+            {logos.map((logo) => (
+              <div className="flex justify-center flex-col items-center">
+                <img className="icon" src={gitContenLikn + logo} alt="" />
+                <p className="text-xs mt-2">
+                  {logo.split(".").length > 1 ? logo.split(".")[0] : ""}
+                </p>
+              </div>
+            ))}
           </div>
         </div>
         <div className="w-1/4 ">
